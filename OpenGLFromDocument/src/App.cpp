c2fd1da877e1d7c4320fe665d13d3c3313a79d3f
Berker Turan <turanberker@yahoo.com>
2022-03-19 16:53:55 +0300

    Part1.HelloWindow

--- OpenGLFromDocument/src/App.cpp
+++ OpenGLFromDocument/src/App.cpp
@@ -6,7 +6,52 @@
 
 using namespace std;
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	//esc ye basıldığında yapılacak işlem tarif edilir
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+
 int main(void) {
-	cout << "Hello" << endl		;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "Failed to initialize GLAD" << endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	//Wİndow her resize olduğunda aşağıdaki fonksiyon çağrılacaktır
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	
+	while (!glfwWindowShouldClose(window)) {
+		
+		processInput(window);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	
+	}
+	glfwTerminate();
 	return 0;
 }
