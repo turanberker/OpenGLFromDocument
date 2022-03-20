073ca98907fe0ac802b89ff52f44c70f2e2156e1
Berker Turan <turanberker@yahoo.com>
2022-03-20 15:28:33 +0300

    Element Buffer Objects

--- OpenGLFromDocument/src/App.cpp
+++ OpenGLFromDocument/src/App.cpp
@@ -24,12 +24,12 @@ const char* vertexShaderSource = "#version 330 core\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
 
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
 
 int main(void) {
 	glfwInit();
@@ -53,9 +53,9 @@ int main(void) {
 
 	glViewport(0, 0, 800, 600);
 	//Wİndow her resize olduğunda aşağıdaki fonksiyon çağrılacaktır
-	
 
-	unsigned int vertexShader= glCreateShader(GL_VERTEX_SHADER);
+
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
 	int success;
@@ -66,7 +66,7 @@ int main(void) {
 		std::cout << "ERROR::SHADER::VERTEX:COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
 
-	unsigned int fragmenShader= glCreateShader(GL_FRAGMENT_SHADER);
+	unsigned int fragmenShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmenShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmenShader);
 
@@ -92,29 +92,39 @@ int main(void) {
 
 
 	float vertices[] = {
-		   -0.5f, -0.5f, 0.0f, // left  
-			0.5f, -0.5f, 0.0f, // right 
-			0.0f,  0.5f, 0.0f  // top   
+		0.5f, 0.5f, 0.0f, // top right
+		0.5f, -0.5f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f, // bottom left
+		-0.5f, 0.5f, 0.0f // top left
 	};
 
-	unsigned int VAO,VBO;
+	unsigned int indices[] = {
+		0,1,3,
+		1,2,3
+	};
+
+	unsigned int VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
 
-	
+
 	//Copy our vertices array in a bufer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	//set the certex attrivbutes pointers
+	//copy our index array in a element buffer for OpenGL to use
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//set the vertex attrivbutes pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	while (!glfwWindowShouldClose(window)) {
 
 		processInput(window);
@@ -122,7 +132,11 @@ int main(void) {
 		glClear(GL_COLOR_BUFFER_BIT);
 		// draw our first triangle
 		glUseProgram(shaderProgram);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		//	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
